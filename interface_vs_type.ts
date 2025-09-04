29a783e3754558cdf059418e8cb2dad917f2094e
saurabhvybs <psaurabh51@gmail.com>
2025-09-04 23:19:23 +0530

    update interface vs types

--- interface_vs_type.ts
+++ interface_vs_type.ts
@@ -15,6 +15,12 @@ interface User {
 
 console.log("Interface Merging : ", {name : "John", email : "john@example.com", role : "admin"});
 
+
+
+
+
+// Now let's see how we can achieve similar functionality with type aliases.
+
 type Product = {
     id : number,
     name : string
@@ -29,3 +35,62 @@ type ProductDetails = Product & {
 // but remember that intersection types create a new type, they don't modify the existing type.
 const product: ProductDetails = {id : 1, name : "Laptop", price : 1000};
 console.log("Type Alias with Intersection : ", product);
+
+
+
+
+
+
+
+// Another difference is that interfaces can be implemented by classes, while type aliases cannot.
+// This makes interfaces more suitable for defining contracts that classes must adhere to.
+
+// Example of interface being implemented by a class
+interface CanWalk {
+  walk(): void;
+}
+
+class Person implements CanWalk { // ✅ Correct and idiomatic
+  walk() {
+    console.log("Walking...");
+  }
+}
+
+// Example of type alias being used with a class
+// Here we define a type alias for an object type
+type CanTalk = {
+  talk(): void;
+};
+
+type StringOrNumber = string | number;
+
+class Speaker implements CanTalk { // ✅ Works, but less common
+  talk() {
+    console.log("Talking...");
+  }
+}
+
+// class Thing implements StringOrNumber {} // 🛑 Error: A class can only implement an object type or intersection of object types.
+
+
+
+
+// creating an Interface
+interface New_User {
+  id: number;
+  name: string;
+}
+
+// extending the interface New_User to create a new interface Admin
+// this is useful when you want to create a more specific type based on an existing one, without tweaking the original type.
+interface Admin extends New_User {
+  permissions: string[];
+}
+
+const adminUser: Admin = {
+  id: 1,
+  name: "Jane Doe",
+  permissions: ["read", "write"]
+};
+
+//but we can't use extends with type aliases directly.
